4e606fc20e6487e46883541acd0cabbc3991175a
pra~kásh <118615580+Prakashkhatiwada@users.noreply.github.com>
2025-02-25 20:52:04 +0545

    Update db.php

--- db.php
+++ db.php
@@ -1,15 +1,9 @@
 <?php
-// filepath: /C:/xampp/htdocs/CMSproject/db.php
-
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "cmsproject";
-
-// Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
-
-// Check connection
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
